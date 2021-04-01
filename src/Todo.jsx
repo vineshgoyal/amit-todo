8a131a37695ade7598332c1ad504e1f55263c376
Amit <itsamit622@gmail.com>
2021-04-01 11:05:35 +0530

    textdecoration&checkbox

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,21 +4,27 @@ import React from 'react'
 export default class Todo extends React.Component {
 clickme2(){
     console.log("onclick ",  this.props.onClick)
-        this.props.onClick(this.props.id ,this.props.index)
+    
+        this.props.onClick(this.props.id )
+        
 
 }
 Clicked(event){
-    console.log("Val", event.target.value)
-    this.props.onClick2(this.props.complete)
+    console.log("Val", this.props.todo)
+    this.props.onClick2(this.props.todo , event)
 }
 
     render(){
-        console.log(this.props)
+        let checkedClass="";
+        if(this.props.complete){
+            checkedClass="checked-item"
+        }
+       
         return  <div>
-        <h1 className="list-group-item list-group-item-action list-group-item-info"><input type="checkbox" onChange={this.Clicked.bind(this)}/> {this.props.title}
-        <img src="./images/2.png" onClick={this.clickme2.bind(this)} />
-        
+        <h1 className="list-group-item list-group-item-action list-group-item-info " ><span className={checkedClass}><input type="checkbox" checked={this.props.complete} onChange={this.Clicked.bind(this)}/> {this.props.title}</span>
+        <img src="./images/3.png" style={{float:'right' ,width:"60px"}} onClick={this.clickme2.bind(this)} />
         </h1>
+        
         </div>
     }
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -8,7 +8,7 @@ export default class TodoList extends React.Component {
 
   state = {
     Users: [],
-    complete: false,
+   
     temptitle: "",
   }
 
@@ -25,7 +25,7 @@ export default class TodoList extends React.Component {
   }
   getList() {
     return this.state.Users.map((values, i) => {
-      return <Todo index={i} key={this.props.id} complete={values.complete} id={values.id} title={values.title} onClick={this.ClickMe.bind(this)} onClick2={this.ClickMe2}/>
+      return <Todo todo={values} complete={values.complete} index={i} key={this.props.id} complete={values.complete} id={values.id} title={values.title} onClick={this.ClickMe.bind(this)} onClick2={this.ClickMe2} />
     })
   }
   CaptureValue(event) {
@@ -35,43 +35,51 @@ export default class TodoList extends React.Component {
     })
     console.log(this.state.Users)
   }
-  ClickMe(value,i){
-    console.log('ok',value.id)
-
-   
-    let temp =[...this.state.Users]
-    
+  ClickMe(value){
+    console.log('ok',value)
     HttpsReq.delete("todos/" + value).then((res) => {
-         console.log(res.data);
-        temp.splice(i,1)
-         this.setState({
-           Users:temp
-         })
-         
-  })
+      console.log(res.data);   
+})
+    let index =null;
+    let temp =[...this.state.Users]
+  for(let i=0;i<this.state.Users.length;i++){
+    if(this.state.Users[i].id==value){
+     index =i;
+     console.log(index)
+     temp.splice(index,1)
+     this.setState({
+      Users:temp
+    })
+    break
+    }
+      
+ 
+  }
+    
+  /*temp.splice(index,1)
+  this.setState({
+    Users:temp
+  })*/
     
   }
-  ClickMe2=(value)=>{
-    let newtodo = {
-      
-      complete:value
+  ClickMe2=(todo ,event)=>{
+    console.log(event.target.checked)
+   let checked=event.target.checked
+   HttpsReq.patch("todos/" + todo.id , {complete : checked}).then((res)=>{
+     console.log(todo.id)
+    let todoList=[...this.state.Users];
+    let index =null;
+    for(let i=0;i<this.state.Users.length;i++){
+     if(this.state.Users[i].id==todo.id){
+      index =i;
+      console.log(index)
+      this.state.Users[index].complete=checked
+      this.setState(this.state)
     }
-    let user2 =this.state.Users
-    if(value){
-    HttpsReq.post("/todos", newtodo).then((res) => {
-      console.log(res.data)
-      user2.push(res.data)
-      this.setState({
-        users:user2,
-        temptitle :""
-      })
-      
-    })
+    
+   }
+   })
   }
-  }
-  
-
-
   updatebtn() {
 
     let newtodo = {
@@ -98,19 +106,27 @@ export default class TodoList extends React.Component {
 
   Userdetails() {
 
-    return <h1><input type="text" placeholder="Add your new todo" value={this.state.temptitle} onChange={this.CaptureValue.bind(this)} />
-      <img src="./images/1.png" onClick={this.updatebtn.bind(this)} /></h1>
+    return <div>
+    <h1>
+      <input type="text" placeholder="Add your new todo" value={this.state.temptitle} onChange={this.CaptureValue.bind(this)} />
+      <img src="./images/4.png" style={{float:"right" , width:"60px"}} onClick={this.updatebtn.bind(this)} />
+      </h1>
+      </div>
 
   }
   render() {
 
-
+       let button;
+       if(this.state.temptitle!=undefined){
+         button =<button className="btnclass">Clear All</button>
+       }
     console.log(this.state)
     let count;
     if (this.state.Users.length > 0) {
       count = <p>You have {this.state.Users.length} pending value</p>
     }
     return <div className=" class2">
+      <div className="class3">
       <h1>TODO APP</h1>
 
       {this.Userdetails()}
@@ -118,8 +134,10 @@ export default class TodoList extends React.Component {
       <div class="list">
 
         {this.getList()}
+        
       </div>
-
+      <div >{button}</div>
+      </div>
     </div>
 
 
